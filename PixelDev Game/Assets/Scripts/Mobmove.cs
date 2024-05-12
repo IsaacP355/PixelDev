7848dfc4cc67b1fc8f97b10ae0e26cccc36c2a9f
IsaacP355 <pitts81804@gmail.com>
2024-05-11 23:22:51 -0700

    Update on both level 1 and 2

--- PixelDev Game/Assets/Scripts/Mobmove.cs
+++ PixelDev Game/Assets/Scripts/Mobmove.cs
@@ -7,23 +7,17 @@ public class Mobmove : MonoBehaviour
     public GameObject player;
     public float speed;
     public float distanceBetween;
-
-
     private float distance;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
+        float distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
 
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        angle -= 180f;
 
         if(distance < distanceBetween)
         {
