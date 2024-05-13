2b64659b4a8a1fa85508955704afa31279ae3e92
NoBugsHappyLife <158510066+NoBugsHappyLife@users.noreply.github.com>
2024-05-13 12:51:16 -0700

    Edited Play Button
    If the user has already beaten the slime at the end of level 1, if they ever log back in the play button will instead direct them to Level 2. They will not have to replay all of Level 1.

--- PixelDev Game/Assets/MainMenuArt/Scripts/MainMenu.cs
+++ PixelDev Game/Assets/MainMenuArt/Scripts/MainMenu.cs
@@ -1,13 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Firebase.Auth;
+using Firebase.Database;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] GameObject AudioMenu;
     [SerializeField] GameObject SettingsMenu;
-    public void GoToScene(string sceneName)
+    private DatabaseReference dbRef;
+    private bool Level1Clear = false;
+    public async void GoToScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        FirebaseUser user = FirebaseAuth.DefaultInstance.CurrentUser;
+        dbRef = FirebaseDatabase.DefaultInstance.GetReference("users/" + FirebaseAuth.DefaultInstance.CurrentUser.UserId + "/Level1Clear"); 
+
+        DataSnapshot IsLevel1Clear = await dbRef.GetValueAsync();
+        if (IsLevel1Clear.Exists)
+            Level1Clear = (bool)IsLevel1Clear.Value; // if it is inside the table, Level1Clear should be setting to true
+        else
+            Debug.Log("User has not completed the first level");
+
+        if (Level1Clear == true) // this means the user has already cleared Level 1
+            SceneManager.LoadScene("Level 2");
+        else
+            SceneManager.LoadScene(sceneName); 
     }
 
     public void QuitApp()
